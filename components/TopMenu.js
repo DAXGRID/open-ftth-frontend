5c68bcfd73dccb89fe622fa27a91ce077bea7e1b
Rune Nielsen <runeanielsen@live.dk>
2020-08-20 14:02:59 +0200

    implements default layout structure

---
+++ components/TopMenu.js
@@ -0,0 +1,9 @@
+const TopMenu = () => {
+  return (
+    <div>
+      <h1>Top-menu</h1>
+    </div>
+  );
+};
+
+export default TopMenu;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import TopMenu from "../components/TopMenu";
 
 export default function Home() {
   return (
-    <div>
-      <h1>OPEN-FTTH</h1>
+    <div className={styles.container}>
+      <Head>
+        <title>OPEN-FTTH</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <TopMenu />
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Body</h1>
+      </main>
     </div>
   );
 }
