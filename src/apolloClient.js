f5e10e6e1a03d2a040cabd2a8cf7182695e86386
Rune Nielsen <runeanielsen@live.dk>
2020-08-31 22:19:36 +0200

    adds subscription for geographical area updated

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,33 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
+
+const httpLink = new HttpLink({
+  uri: "http://10.111.141.70/graphql",
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://10.111.141.70/graphql`,
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const apolloClient = new ApolloClient({
-  uri: "http://10.109.87.197/graphql",
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,13 +1,40 @@
 import React from "react";
 import NamingInfo from "../components/NamingInfo";
 import FormSection from "../components/FormSection";
+import { gql, useSubscription } from "@apollo/client";
+
+const SUBSCRIBE_GEOGRAPHICAL_AREA_UPDATED_EVENTS = gql`
+  subscription {
+    geographicalAreaUpdatedEvents {
+      _id: eventId
+      __typename
+    }
+  }
+`;
 
 function IndexPage() {
+  const { data, loading } = useSubscription(
+    SUBSCRIBE_GEOGRAPHICAL_AREA_UPDATED_EVENTS,
+    {
+      fetchPolicy: "no-cache",
+      onSubscriptionData: write,
+    }
+  );
+
+  function write(message) {
+    if (window.printer) {
+      window.printer.text(JSON.stringify(message.subscriptionData.data));
+    }
+  }
+
   return (
     <div>
       <FormSection>
         <NamingInfo />
       </FormSection>
+      <p>
+        {!loading && data.geographicalAreaUpdatedEvents.eventSequenceNumber}
+      </p>
     </div>
   );
 }
