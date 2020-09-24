545b247c33fabb8e4927e218f6fdb2416d3c6d2f
Rune Nielsen <runeanielsen@live.dk>
2020-09-24 14:29:04 +0200

    removes the binding to injected javascript

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,30 +1,6 @@
 import React from "react";
-import { gql, useSubscription } from "@apollo/client";
-
-const SUBSCRIBE_GEOGRAPHICAL_AREA_UPDATED_EVENTS = gql`
-  subscription {
-    geographicalAreaUpdatedEvents {
-      _id: eventId
-      __typename
-    }
-  }
-`;
 
 function IndexPage() {
-  const { data, loading } = useSubscription(
-    SUBSCRIBE_GEOGRAPHICAL_AREA_UPDATED_EVENTS,
-    {
-      fetchPolicy: "no-cache",
-      onSubscriptionData: write,
-    }
-  );
-
-  function write(message) {
-    if (window.printer) {
-      window.printer.text(JSON.stringify(message.subscriptionData.data));
-    }
-  }
-
   return <div></div>;
 }
 
