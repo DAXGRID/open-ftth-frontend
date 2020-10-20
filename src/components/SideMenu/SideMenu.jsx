59dada00a75f294fd0dabb18e0ef04cb8eb6dbe3
Rune Nielsen <runenielsen@runbox.com>
2020-10-20 13:19:30 +0200

    updates selection of side-menu based on links

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { faHome, faProjectDiagram } from "@fortawesome/free-solid-svg-icons";
+import { Link, useLocation } from "react-router-dom";
+
+function SideMenuItem({ path, linkText, icon }) {
+  const location = useLocation();
+
+  return (
+    <li
+      className={
+        location.pathname === path
+          ? "side-menu-item side-menu-item--selected"
+          : "side-menu-item"
+      }
+    >
+      <Link to={path}>
+        <span className="side-menu-item-icon">
+          <FontAwesomeIcon icon={icon} />{" "}
+        </span>
+        {linkText}
+      </Link>
+    </li>
+  );
+}
 
 const SideMenu = ({ open }) => {
   return (
     <div className={open ? "side-menu show" : "side-menu"}>
       <ul>
-        <li className="side-menu-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="side-menu-item">
-          <Link to="/route-segment">Route segment</Link>
-        </li>
+        <SideMenuItem path="/" linkText="Home" icon={faHome} />
+        <SideMenuItem
+          path="/route-segment"
+          linkText="Route segment"
+          icon={faProjectDiagram}
+        />
       </ul>
     </div>
   );
