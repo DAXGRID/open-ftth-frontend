8d020299720a7607185c091d945bd90cd5fa67cb
Rune Nielsen <runenielsen@runbox.com>
2020-11-06 11:53:25 +0100

    adds translations for side-menu

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
 import Diagram from "../../components/Diagram";
 
 function IndexPage() {
-  const { t } = useTranslation();
-
   return (
     <div className="index-page">
       <Diagram />
