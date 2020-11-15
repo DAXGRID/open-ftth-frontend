15ca0fd4de03ed9b1a27a0313fba47cbbd74900a
Rune Nielsen <runenielsen@runbox.com>
2020-11-15 23:50:15 +0100

    adds initial layout for worktasks page

--- src/pages/WorkTaskPage/WorkTaskPage.jsx
+++ src/pages/WorkTaskPage/WorkTaskPage.jsx
@@ -1,9 +1,129 @@
-import React from "react";
+import React, { useState } from "react";
+import SelectListView from "../../components/SelectListView";
+import { useTranslation } from "react-i18next";
+import DefaultButton from "../../components/DefaultButton";
 
 function WorkTaskPage() {
+  const { t } = useTranslation();
+  const [worktasks, setWorkTasks] = useState([
+    {
+      rows: [
+        "GM Plast One",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+      ],
+      id: 1,
+      selected: false,
+    },
+    {
+      rows: [
+        "GM Plast One",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+      ],
+      id: 1,
+      selected: false,
+    },
+    {
+      rows: [
+        "GM Plast One",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+      ],
+      id: 1,
+      selected: false,
+    },
+    {
+      rows: [
+        "GM Plast One",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+      ],
+      id: 1,
+      selected: false,
+    },
+    {
+      rows: [
+        "GM Plast One",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+      ],
+      id: 1,
+      selected: false,
+    },
+    {
+      rows: [
+        "GM Plast One",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+        "Oe50 7x16",
+      ],
+      id: 1,
+      selected: false,
+    },
+  ]);
+
+  const selectItem = (selectedItem) => {
+    conduits.forEach((x) => (x.selected = false));
+    selectedItem.selected = true;
+    setConduits([...conduits]);
+  };
+
   return (
     <div>
-      <h1>Work tasks</h1>
+      <div className="full-row">
+        <SelectListView
+          headerItems={[
+            t("Installation Id"),
+            t("Central office area"),
+            t("Flex point area"),
+            t("Splice point area"),
+            t("Technology"),
+            t("Work task type"),
+            t("Address"),
+            t("Status"),
+          ]}
+          bodyItems={worktasks}
+          selectItem={selectItem}
+        />
+      </div>
+
+      <div className="full-row">
+        <DefaultButton
+          maxWidth="400px"
+          innerText={t("Pan/Zoom to address")}
+          onClick={() => {}}
+        />
+      </div>
     </div>
   );
 }
