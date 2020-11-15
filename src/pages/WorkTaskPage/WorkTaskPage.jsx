85bda2008b57d6a51a6f7c1848473d877bdb534e
Rune Nielsen <runenielsen@runbox.com>
2020-11-15 16:07:33 +0100

    adds structure for work task page

---
+++ src/pages/WorkTaskPage/WorkTaskPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WorkTaskPage() {
+  return (
+    <div>
+      <h1>Work tasks</h1>
+    </div>
+  );
+}
+
+export default WorkTaskPage;
---
+++ src/pages/WorkTaskPage/index.jsx
@@ -0,0 +1,4 @@
+import WorkTaskPage from "./WorkTaskPage";
+import "./WorkTaskPage.scss";
+
+export default WorkTaskPage;
