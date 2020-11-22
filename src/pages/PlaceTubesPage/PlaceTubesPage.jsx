7b297539ee06e0b7c2b9c15f46072d863ab5600a
Rune Andreas Nielsen <runeanielsen@users.noreply.github.com>
2020-11-22 11:06:41 +0100

    changes the name of conduits for mock demo (#14)

--- src/pages/PlaceTubesPage/PlaceTubesPage.jsx
+++ src/pages/PlaceTubesPage/PlaceTubesPage.jsx
@@ -12,10 +12,12 @@ function PlaceTubesPage() {
   const { retrieveSelected } = useBridgeConnector();
   const [validation, setValidation] = useState({});
   const [conduits, setConduits] = useState([
-    { rows: ["GM Plast One", "Oe50 7x16"], id: 1, selected: false },
-    { rows: ["GM Plast Two", "Oe50 7x16"], id: 2, selected: false },
-    { rows: ["GM Plast Three", "Oe50 7x16"], id: 3, selected: false },
-    { rows: ["GM Plast Four", "Oe50 7x16"], id: 4, selected: false },
+    { rows: ["Emtelle", "ø40 tomrør"], id: 1, selected: false },
+    { rows: ["Emtelle", "ø50 tomrør"], id: 2, selected: false },
+    { rows: ["Emtelle", "ø110 tomrør"], id: 3, selected: false },
+    { rows: ["Emtelle", "ø40 5x10/8"], id: 4, selected: false },
+    { rows: ["Emtelle", "ø40 6x10/8"], id: 5, selected: false },
+    { rows: ["Emtelle", "ø50 10x10/8"], id: 6, selected: false },
   ]);
 
   useEffect(() => {
