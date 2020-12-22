f7ffd47fdf14515d98f85c75b17dd6883b8607fc
Rune Nielsen <runenielsen@runbox.com>
2020-12-22 16:07:47 +0100

    adds highlight features bridge function

--- src/bridge/useBridgeConnector.js
+++ src/bridge/useBridgeConnector.js
@@ -16,7 +16,21 @@ function useBridgeConnector() {
     send(message);
   }
 
-  return { retrieveSelected, panToCoordinate };
+  function highlightFeatures() {
+    const message = {
+      eventType: "HighlightFeatures",
+      IdentifiedFeatureMrids: [
+        "b92d9f91-1a08-4f49-932a-e64f9cef756b",
+        "b92d9f91-1a08-4f49-932a-e64f9cef756b",
+      ],
+      featureType: "RouteSegment",
+      username: "user",
+    };
+
+    send(message);
+  }
+
+  return { retrieveSelected, panToCoordinate, highlightFeatures };
 }
 
 export default useBridgeConnector;
