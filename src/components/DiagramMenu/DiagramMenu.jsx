c2525acb2187dfe42edf463c6682e6fa18f09116
Rune Nielsen <runenielsen@runbox.com>
2021-01-07 02:26:13 +0100

    moved toggle button to its own function

--- src/components/DiagramMenu/DiagramMenu.jsx
+++ src/components/DiagramMenu/DiagramMenu.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faSearchLocation,
@@ -6,18 +7,30 @@ import {
   faCut,
 } from "@fortawesome/free-solid-svg-icons";
 
+function ToggleButton({ icon }) {
+  return (
+    <span className="icon-toggle-button">
+      <FontAwesomeIcon icon={icon} />
+    </span>
+  );
+}
+
+ToggleButton.propTypes = {
+  icon: PropTypes.shape(
+    PropTypes.shape({
+      icon: PropTypes.arrayOf(PropTypes.array),
+      iconName: PropTypes.string,
+      prefix: PropTypes.string,
+    }).isRequired
+  ).isRequired,
+};
+
 function DiagramMenu() {
   return (
     <div className="diagram-menu">
-      <span className="icon-toggle-button">
-        <FontAwesomeIcon icon={faSearchLocation} />
-      </span>
-      <span className="icon-toggle-button">
-        <FontAwesomeIcon icon={faHighlighter} />
-      </span>
-      <span className="icon-toggle-button">
-        <FontAwesomeIcon icon={faCut} />
-      </span>
+      <ToggleButton icon={faSearchLocation} />
+      <ToggleButton icon={faHighlighter} />
+      <ToggleButton icon={faCut} />
     </div>
   );
 }
