b59c0ba5a99d49627f63492d5a787dbc8442c5ca
Rune Nielsen <runenielsen@runbox.com>
2021-01-21 22:35:33 +0100

    simplified the login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,5 @@
-import * as React from "react";
-import { useCallback } from "react";
+import React from "react";
 import { Redirect, useLocation } from "react-router-dom";
-
 import { useKeycloak } from "@react-keycloak/web";
 
 const LoginPage = () => {
@@ -14,20 +12,12 @@ const LoginPage = () => {
 
   if (!initialized) return <div>Loading</div>;
 
-  const login = useCallback(() => {
-    keycloak.login();
-  }, [keycloak]);
-
   if (keycloak.authenticated)
     return <Redirect to={currentLocationState.from} />;
 
-  return (
-    <div>
-      <button type="button" onClick={login}>
-        Login
-      </button>
-    </div>
-  );
+  keycloak.login();
+
+  return <></>;
 };
 
 export default LoginPage;
