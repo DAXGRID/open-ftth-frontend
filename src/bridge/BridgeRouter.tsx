63c4e1dc2da318a33141acde9b2c0f8739d65153
Rune Nielsen <runenielsen@runbox.com>
2021-01-26 13:49:11 +0100

    adds type for identifynetworkevent

--- src/bridge/BridgeRouter.tsx
+++ src/bridge/BridgeRouter.tsx
@@ -2,13 +2,20 @@ import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import PubSub from "pubsub-js";
 
+type IdentifyNetworkEvent = {
+  eventType: string;
+  identifiedFeatureId: string;
+  selectedType: string;
+  username: string;
+};
+
 function BridgeRouter() {
   const history = useHistory();
 
   useEffect(() => {
     const token = PubSub.subscribe(
       "IdentifyNetworkElement",
-      (_msg: string, data: any) => {
+      (_msg: string, data: IdentifyNetworkEvent) => {
         history.push(`/identify-feature/${data.identifiedFeatureId}`);
       }
     );
@@ -16,7 +23,7 @@ function BridgeRouter() {
     return () => {
       PubSub.unsubscribe(token);
     };
-  }, []);
+  }, [history]);
 
   return <></>;
 }
