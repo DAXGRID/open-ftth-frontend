250b5a53be28fb43197527fe3bbff42f1cfb42cd
Rune Nielsen <runenielsen@runbox.com>
2021-02-07 00:18:31 +0100

    splits api-gateway into ws and http

--- public/config-env_copy.js
+++ public/config-env_copy.js
@@ -1,5 +1,6 @@
 window.config_MAPBOX_API_KEY = "";
 window.config_MAPBOX_STYLE_URL = "";
-window.config_API_GATEWAY_URI = "";
+window.config_API_GATEWAY_HTTP_URI = "";
+window.config_API_GATEWAY_WS_URI = "";
 window.config_DESKTOP_BRIDGE_URI = "";
 window.config_KEYCLOAK_URI = "";
--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,8 @@ declare global {
   interface Window {
     config_MAPBOX_API_KEY: string;
     config_MAPBOX_STYLE_URL: string;
-    config_API_GATEWAY_URI: string;
+    config_API_GATEWAY_HTTP_URI: string;
+    config_API_GATEWAY_WS_URI: string;
     config_DESKTOP_BRIDGE_URI: string;
     config_KEYCLOAK_URI: string;
   }
@@ -11,7 +12,8 @@ declare global {
 const settings = {
   MAPBOX_API_KEY: window.config_MAPBOX_API_KEY,
   MAPBOX_STYLE_URI: window.config_MAPBOX_STYLE_URL,
-  API_GATEWAY_URI: window.config_API_GATEWAY_URI,
+  API_GATEWAY_HTTP_URI: window.config_API_GATEWAY_HTTP_URI,
+  API_GATEWAY_WS_URI: window.config_API_GATEWAY_WS_URI,
   DESKTOP_BRIDGE_URI: window.config_DESKTOP_BRIDGE_URI,
   KEYCLOAK_URI: window.config_KEYCLOAK_URI,
 };
