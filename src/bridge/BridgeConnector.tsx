3d8ffae0560466f13620e5683726dc272eb27bfa
Rune Nielsen <runenielsen@runbox.com>
2021-02-26 22:55:07 +0100

    try reconnect to bridge-connector when disconnected

--- src/bridge/BridgeConnector.tsx
+++ src/bridge/BridgeConnector.tsx
@@ -11,20 +11,37 @@ function send(eventMsg: any) {
 
 function BridgeConnector() {
   useEffect(() => {
-    client = new w3cwebsocket(Config.DESKTOP_BRIDGE_URI);
+    function setup() {
+      client = new w3cwebsocket(Config.DESKTOP_BRIDGE_URI);
 
-    client.onmessage = (message: any) => {
-      const event = JSON.parse(message.data);
-      PubSub.publish(event.eventType, event);
-    };
+      client.onmessage = (message: any) => {
+        const event = JSON.parse(message.data);
+        PubSub.publish(event.eventType, event);
+      };
 
-    client.onopen = () => {
-      console.log("Connected");
-    };
+      client.onopen = () => {
+        console.log("Connected to BridgeConnector");
+      };
 
-    client.onclose = () => {
-      console.log("Disconnected");
-    };
+      client.onclose = () => {
+        console.log("Disconnected from BridgeConnector");
+        reconnect();
+      };
+
+      client.onerror = () => {
+        console.log("Error happend in BridgeConnector");
+        reconnect();
+      };
+
+      function reconnect() {
+        setTimeout(() => {
+          console.log("Reconnecting to BrideConnector");
+          setup();
+        }, 5000);
+      }
+    }
+
+    setup();
 
     return () => {
       client = null;
