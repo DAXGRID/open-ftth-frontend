935a4ebdb18870cb2f6fdfd6b69e522192e7196e
Rune Nielsen <runenielsen@runbox.com>
2021-03-02 21:57:53 +0100

    basic setup for mapbox

--- src/components/SchematicDiagram/SchematicDiagram.tsx
+++ src/components/SchematicDiagram/SchematicDiagram.tsx
@@ -1,5 +1,33 @@
+import { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import Config from "../../config";
+
+mapboxgl.accessToken = Config.MAPBOX_API_KEY;
+
 function SchematicDiagram() {
-  return <div></div>;
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const [lng] = useState(0.014);
+  const [lat] = useState(0.014);
+  const [zoom] = useState(14);
+  const [minZoom] = useState(12);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current === null ? "" : mapContainer.current,
+      style: Config.MAPBOX_STYLE_URI,
+      center: [lng, lat],
+      zoom: zoom,
+      minZoom: minZoom,
+    });
+
+    return () => map.remove();
+  }, []);
+
+  return (
+    <div className="schematic-diagram">
+      <div className="schematic-diagram-container" ref={mapContainer} />
+    </div>
+  );
 }
 
 export default SchematicDiagram;
