0c924a71e62109e4c127a7c91a659230bbf3a27d
Rune Nielsen <runenielsen@runbox.com>
2021-03-30 16:21:36 +0200

    adds color options to config

--- public/config-env_copy.js
+++ public/config-env_copy.js
@@ -2,3 +2,4 @@ window.config_API_GATEWAY_HTTP_URI = "";
 window.config_API_GATEWAY_WS_URI = "";
 window.config_DESKTOP_BRIDGE_URI = "";
 window.config_KEYCLOAK_URI = "";
+window.config_COLOR_OPTIONS = ["Blue tape", "Red tape", "Black-Black tape"];
