c41d5bad16695d330b57b708ad4f9abcdc360fdc
Rune Nielsen <runenielsen@runbox.com>
2021-04-01 19:48:20 +0200

    adds updated icons (#110)

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,17 +1,23 @@
-import CutConduitSvg from "./cut-conduit.svg";
-import PencilSvg from "./pencil.svg";
-import PlusSvg from "./plus.svg";
-import DisconnectSvg from "./disconnect.svg";
+import AddConduitSvg from "./add-conduit.svg";
+import AddStructureSvg from "./add-structure.svg";
 import ConnectSvg from "./connect.svg";
+import CutConduitSvg from "./cut-conduit.svg";
+import DisconnectSvg from "./disconnect.svg";
+import FlipSvg from "./flip.svg";
+import Loading from "./loading.svg";
+import PencilSvg from "./pencil.svg";
 import PutInContainerSvg from "./put-in-container.svg";
 import RemoveFromContainerSvg from "./remove-from-container.svg";
 import TrashCanSvg from "./trash-can.svg";
 
 export {
+  Loading,
   CutConduitSvg,
   PencilSvg,
-  PlusSvg,
+  AddConduitSvg,
+  AddStructureSvg,
   DisconnectSvg,
+  FlipSvg,
   ConnectSvg,
   PutInContainerSvg,
   RemoveFromContainerSvg,
