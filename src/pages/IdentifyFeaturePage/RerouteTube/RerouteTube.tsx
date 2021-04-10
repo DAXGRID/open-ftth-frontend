6ad31c01eb54d9ddc1f64cca7869ddd1fc60c880
Rune Nielsen <runenielsen@runbox.com>
2021-04-10 16:58:06 +0200

    updates translations

--- src/pages/IdentifyFeaturePage/RerouteTube/RerouteTube.tsx
+++ src/pages/IdentifyFeaturePage/RerouteTube/RerouteTube.tsx
@@ -3,6 +3,8 @@ import DefaultButton from "../../../components/DefaultButton";
 import { useClient, Client } from "urql";
 import useBridgeConnector from "../../../bridge/useBridgeConnector";
 import { MapContext } from "../../../contexts/MapContext";
+import { toast } from "react-toastify";
+import { TFunction, useTranslation } from "react-i18next";
 import {
   QUERY_GET_ROUTESEGMENT_IDS,
   GetRouteSegmentIdsParameter,
@@ -19,7 +21,8 @@ type RerouteTubeParams = {
 const selectRouteSegmentsInMap = async (
   id: string,
   client: Client,
-  selectRouteSegments: (mrids: string[]) => void
+  selectRouteSegments: (mrids: string[]) => void,
+  t: TFunction<string>
 ) => {
   const params: GetRouteSegmentIdsParameter = {
     spanEquipmentOrSegmentId: id,
@@ -29,14 +32,18 @@ const selectRouteSegmentsInMap = async (
     .query<GetRouteSegmentIdsResponse>(QUERY_GET_ROUTESEGMENT_IDS, params)
     .toPromise();
 
-  const mrids = result.data?.utilityNetwork.spanEquipment.routeSegmentIds;
+  const mrids =
+    result.data?.utilityNetwork?.spanEquipment?.routeSegmentIds ?? [];
   selectRouteSegments(mrids ?? []);
+
+  toast.success(t("SELECTED"));
 };
 
 const reroute = async (
   id: string,
   routeSegmentIds: string[],
-  client: Client
+  client: Client,
+  t: TFunction<string>
 ) => {
   const params: RerouteParameter = {
     spanEquipmentOrSegmentId: id,
@@ -46,31 +53,41 @@ const reroute = async (
   const result = await client
     .mutation<RerouteResponse>(MUTATION_REROUTE, params)
     .toPromise();
+
+  if (result.data?.spanEquipment.move.isSuccess) {
+    toast.success(t("SUCCESS_MOVED"));
+  } else {
+    toast.error(t(result.data?.spanEquipment.move.errorCode ?? "ERROR"));
+  }
 };
 
 function RerouteTube({ selectedRouteSegmentMrid }: RerouteTubeParams) {
   const client = useClient();
   const { selectRouteSegments } = useBridgeConnector();
   const { selectedSegmentIds } = useContext(MapContext);
+  const { t } = useTranslation();
 
   return (
     <div>
-      <DefaultButton
-        onClick={() =>
-          selectRouteSegmentsInMap(
-            selectedRouteSegmentMrid,
-            client,
-            selectRouteSegments
-          )
-        }
-        innerText="Select route segments in map"
-      />
-      <DefaultButton
-        onClick={() =>
-          reroute(selectedRouteSegmentMrid, selectedSegmentIds, client)
-        }
-        innerText="Reroute"
-      />
+      <div className="full-row">
+        <DefaultButton
+          onClick={() =>
+            selectRouteSegmentsInMap(
+              selectedRouteSegmentMrid,
+              client,
+              selectRouteSegments,
+              t
+            )
+          }
+          innerText={t("SELECT")}
+        />
+        <DefaultButton
+          onClick={() =>
+            reroute(selectedRouteSegmentMrid, selectedSegmentIds, client, t)
+          }
+          innerText={t("MOVE")}
+        />
+      </div>
     </div>
   );
 }
