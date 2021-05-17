eed10e794c8e1ea5aeb751ae29b45706abb7f7e8
Rune Nielsen <runenielsen@runbox.com>
2021-05-17 23:24:27 +0200

    adds route network diagram to index page

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,9 +1,11 @@
 import RouteNetworkMap from "../../groups/RouteNetworkMap";
+import RouteNetworkDiagram from "../../groups/RouteNetworkDiagram";
 
 function IndexPage() {
   return (
     <div className="index-page">
       <RouteNetworkMap />
+      <RouteNetworkDiagram />
     </div>
   );
 }
