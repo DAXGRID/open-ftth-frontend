4de710341434d6e2452e497bbe45e8f423e2ebd0
Rune Nielsen <runenielsen@runbox.com>
2021-08-05 13:42:24 +0200

    adds basic layout for work task list

--- src/groups/TopMenu/TopMenu.tsx
+++ src/groups/TopMenu/TopMenu.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faTasks } from "@fortawesome/free-solid-svg-icons";
 import SearchMenu from "./SearchMenu";
 
 type TopMenuProps = {
@@ -11,19 +10,24 @@ function TopMenu({ toggleSideMenu }: TopMenuProps) {
   return (
     <div className="top-menu">
       <div className="top-menu-body">
-        <div
-          className="side-menu-icon"
-          role="button"
-          tabIndex={0}
-          onClick={() => toggleSideMenu()}
-          onKeyPress={(e) =>
-            e.key === "Enter" ? { toggleSideMenu } : () => {}
-          }
-        >
-          <FontAwesomeIcon icon={faBars} />
-        </div>
         <div className="top-menu-item">
+          <div
+            className="side-menu-icon"
+            role="button"
+            tabIndex={0}
+            onClick={() => toggleSideMenu()}
+            onKeyPress={(e) =>
+              e.key === "Enter" ? { toggleSideMenu } : () => {}
+            }
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+        </div>
+        <div className="top-menu-item top-menu-item__center">
           <SearchMenu />
+        </div>
+        <div className="top-menu-item top-menu-item__end">
+          <FontAwesomeIcon icon={faTasks} />
         </div>
       </div>
     </div>
