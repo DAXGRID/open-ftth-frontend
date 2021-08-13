e214f7dc8b11f6629b6e96b5f625c4c0694ac137
Rune Nielsen <runenielsen@runbox.com>
2021-08-13 14:09:43 +0200

    reduces jumping feature information (#188)

--- src/groups/RouteNetworkDiagram/FeatureInformation/FeatureInformation.tsx
+++ src/groups/RouteNetworkDiagram/FeatureInformation/FeatureInformation.tsx
@@ -19,34 +19,34 @@ function FeatureInformation() {
       pause: !identifiedFeature?.id,
     });
 
+  if (routeNetworkElementResponse.fetching) <></>;
+
   const routeElement =
     routeNetworkElementResponse.data?.routeNetwork.routeElement;
 
-  if (!routeElement || !identifiedFeature) return <></>;
-
   return (
     <div className="feature-information">
-      {identifiedFeature.type === "RouteNode" && (
+      {identifiedFeature?.type === "RouteNode" && (
         <div className="feature-informations">
           <p>
             <strong>{t("Name")}</strong>
-            {`: ${routeElement.namingInfo?.name ?? ""}`}
+            {`: ${routeElement?.namingInfo?.name ?? ""}`}
           </p>
           <p>
             <strong>{t("Kind")}</strong>
-            {`: ${t(routeElement.routeNodeInfo?.kind ?? "")}`}
+            {`: ${t(routeElement?.routeNodeInfo?.kind ?? "")}`}
           </p>
           <p>
             <strong>{t("Function")}</strong>
-            {`: ${t(routeElement.routeNodeInfo?.function ?? "")}`}
+            {`: ${t(routeElement?.routeNodeInfo?.function ?? "")}`}
           </p>
         </div>
       )}
-      {identifiedFeature.type === "RouteSegment" && (
+      {identifiedFeature?.type === "RouteSegment" && (
         <div className="feature-informations">
           <p>
             <strong>{t("TYPE")}</strong>
-            {`: ${t(routeElement.routeSegmentInfo?.kind ?? "")}`}
+            {`: ${t(routeElement?.routeSegmentInfo?.kind ?? "")}`}
           </p>
         </div>
       )}
