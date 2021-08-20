1bdf7984e7f9e38c67b359db813116bf2a4e07c2
Rune Nielsen <runenielsen@runbox.com>
2021-08-20 14:12:20 +0200

    updates default layout for terminal equipment

--- src/groups/TerminalEquipment/TerminalEquipment.tsx
+++ src/groups/TerminalEquipment/TerminalEquipment.tsx
@@ -21,24 +21,77 @@ function TableContainer({ children, headerTexts }: TableContainerProps) {
 function ReadonlyTable() {
   return (
     <div className="terminal-equipment-table">
-      <div className="readonly-table-header terminal-equipment-table-grid">
+      <div
+        className="terminal-equipment-header
+        terminal-equipment-table-row
+        terminal-equipment-table-grid"
+      >
         <div className="terminal-equipment-table-item">A-Info</div>
         <div className="terminal-equipment-table-item">From</div>
         <div className="terminal-equipment-table-item">Pin/Port</div>
         <div className="terminal-equipment-table-item">To</div>
         <div className="terminal-equipment-table-item">Z-Info</div>
       </div>
+      <div className="terminal-equipment-table-body">
+        <div className="terminal-equipment-table-row">
+          <div className="terminal-equipment-header-row">
+            <p className="terminal-equipment-row-header__item">1</p>
+            <p className="terminal-equipment-row-header__item">
+              12 soems bred splidsebakke
+            </p>
+            <p className="terminal-equipment-row-header__item">
+              (1 af 12 pladser brugt)
+            </p>
+          </div>
+        </div>
 
-      <div className="terminal-equipment-table-body"></div>
+        <div className="terminal-equipment-table-row">
+          <div className="terminal-equipment-data-row terminal-equipment-table-grid">
+            <div className="terminal-equipment-table-item">
+              GALAH-ODF 1-3-1 WDM 1-4-1 OLT-1-1-1
+            </div>
+            <div className="terminal-equipment-table-item">
+              K102034 (72) Fiber 1
+            </div>
+            <div className="terminal-equipment-table-item">1 -O-</div>
+            <div className="terminal-equipment-table-item">
+              Splitter 1 (1:32) Ind 1
+            </div>
+            <div className="terminal-equipment-table-item">
+              Total 29 installations. 3 free ports
+            </div>
+          </div>
+        </div>
+
+        <div className="terminal-equipment-table-row">
+          <div className="terminal-equipment-data-row terminal-equipment-table-grid">
+            <div className="terminal-equipment-table-item">
+              GALAH-ODF 1-3-1 WDM 1-4-1 OLT-1-1-1
+            </div>
+            <div className="terminal-equipment-table-item">
+              K102034 (72) Fiber 1
+            </div>
+            <div className="terminal-equipment-table-item">2 -O-</div>
+            <div className="terminal-equipment-table-item">
+              Splitter 1 (1:32) Ind 1
+            </div>
+            <div className="terminal-equipment-table-item">
+              Total 29 installations. 3 free ports
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+const testData = {};
 
 function TerminalEquipment() {
   return (
     <div>
       <TableContainer
-        headerTexts={["3 GSS 1", "Comspec FIST-GSS2", "32 bakker"]}
+        headerTexts={["Rack position: 3", "Type: Comspec FIST-GSS2"]}
       >
         <ReadonlyTable />
       </TableContainer>
