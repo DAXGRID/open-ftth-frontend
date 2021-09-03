cbf1bc7e3730395edc5427782e38f97d9842238b
Rune Nielsen <runenielsen@runbox.com>
2021-09-03 14:53:43 +0200

    increase maxhits to 10 for establish customer connection

--- src/groups/RouteNetworkDiagram/EstablishCustomerConnection/EstablishCustomerConnectionGql.ts
+++ src/groups/RouteNetworkDiagram/EstablishCustomerConnection/EstablishCustomerConnectionGql.ts
@@ -67,7 +67,7 @@ query($sourceRouteNodeId: ID!) {
     nearestNeighborNodes(
       sourceRouteNodeId: $sourceRouteNodeId
       maxBirdFlyDistanceMeters: 1000
-      maxHits: 5
+      maxHits: 10
       stops: [CENTRAL_OFFICE_SMALL]
   interests: [CABINET_SMALL, CONDUIT_CLOSURE_BRANCH_OFF]
     ) {
