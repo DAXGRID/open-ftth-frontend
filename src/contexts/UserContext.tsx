f4491f6e8ac1c3aea3fc0af7cfd1589d4076a3f6
Rune Nielsen <runenielsen@runbox.com>
2022-01-20 16:29:26 +0100

    adds roles to user context

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -12,6 +12,7 @@ type UserContextType = {
   userName: string;
   userWorkTask: UserWorkTask | null;
   reloadUserWorkTask: () => void;
+  userRoles: UserRoles;
 };
 
 const UserContext = createContext<UserContextType>({
@@ -20,14 +21,35 @@ const UserContext = createContext<UserContextType>({
   reloadUserWorkTask: () => {
     console.warn("no provider set for reloadUserWorkTask");
   },
+  userRoles: { reader: false, writer: false },
 });
 
 type UserContextProps = {
   children: ReactNode;
 };
 
+type UserRoles = {
+  reader: boolean;
+  writer: boolean;
+};
+
+const AVAILABLE_ROLES = {
+  WRITER: {
+    REALM: "openftth-frontend",
+    ROLE: "writer",
+  },
+  READER: {
+    RESOURCE: "openftth-frontend",
+    ROLE: "reader",
+  },
+};
+
 const UserProvider = ({ children }: UserContextProps) => {
   const [userName, setUsername] = useState<string>("");
+  const [userRoles, setUserRoles] = useState<UserRoles>({
+    reader: false,
+    writer: false,
+  });
   const { initialized, keycloak } = useKeycloak();
   const { userWorkContext, reExecuteUserWorkContextQuery } =
     useUserWorkContext(userName);
@@ -35,11 +57,23 @@ const UserProvider = ({ children }: UserContextProps) => {
   useEffect(() => {
     if (!initialized) return;
     keycloak.loadUserProfile().then(() => {
-      if (!keycloak.profile?.username)
+      console.log(keycloak);
+      if (!keycloak.profile?.username) {
         throw new Error("Could not load user from keycloak.");
-      else setUsername(keycloak.profile.username);
+      } else {
+        setUsername(keycloak.profile.username);
+        const hasRoleReader = keycloak.hasResourceRole(
+          AVAILABLE_ROLES.READER.ROLE,
+          AVAILABLE_ROLES.READER.RESOURCE
+        );
+        const hasRoleWriter = keycloak.hasResourceRole(
+          AVAILABLE_ROLES.WRITER.ROLE,
+          AVAILABLE_ROLES.WRITER.REALM
+        );
+        setUserRoles({ reader: hasRoleReader, writer: hasRoleWriter });
+      }
     });
-  }, [initialized, keycloak]);
+  }, [initialized, keycloak, setUsername, setUserRoles]);
 
   const reloadUserWorkTask = useCallback(() => {
     reExecuteUserWorkContextQuery();
@@ -51,6 +85,7 @@ const UserProvider = ({ children }: UserContextProps) => {
         userName: userName,
         userWorkTask: userWorkContext ?? null,
         reloadUserWorkTask: reloadUserWorkTask,
+        userRoles: userRoles,
       }}
     >
       {children}
