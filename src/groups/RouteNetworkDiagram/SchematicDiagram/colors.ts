b362319c35668b0cca14d13ebed618e3205df64d
Rune Nielsen <runenielsen@runbox.com>
2022-03-15 10:12:12 +0100

    now supports all outer conduit colors (#318)

--- src/groups/RouteNetworkDiagram/SchematicDiagram/colors.ts
+++ src/groups/RouteNetworkDiagram/SchematicDiagram/colors.ts
@@ -1,9 +1,11 @@
 const colorMap = {
   WHITE: "#FFFFFF",
   BROWN: "#996633",
+  LIGHT_BROWN: "#c49566",
   LIGHT_RED: "#FF0000",
   RED: "#cc0000",
   YELLOW: "#ffff00",
+  LIGHT_YELLOW: "#ffff8f",
   LIGHT_BLUE: "#71D3FC",
   BLUE: "#0000FF",
   LIGHT_ORANGE: "#ffc04d",
@@ -12,9 +14,13 @@ const colorMap = {
   LIGHT_GREEN: "#a3ffa3",
   BLACK: "#000000",
   GREY: "#808080",
+  LIGHT_GREY: "#c4c0c0",
   VIOLET: "#9900cc",
+  LIGHT_VIOLET: "#da89f5",
   PINK: "#ff99cc",
+  LIGHT_PINK: "#ffbdde",
   TURQUOISE: "#00ffff",
+  LIGHT_TURQUOISE: "#a8ffff",
 };
 
 export default colorMap;
