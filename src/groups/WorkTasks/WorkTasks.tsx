629d2a72c2d63c0da70a099d4c2791d92d9302f0
Rune Nielsen <runenielsen@runbox.com>
2022-06-10 11:11:52 +0200

    made basic layout for work tasks

--- src/groups/WorkTasks/WorkTasks.tsx
+++ src/groups/WorkTasks/WorkTasks.tsx
@@ -1,205 +1,68 @@
-import { useState, useEffect, useMemo, useCallback, useContext } from "react";
 import { useTranslation } from "react-i18next";
-import { useKeycloak } from "@react-keycloak/web";
-import { useQuery, useMutation } from "urql";
-import { UserContext } from "../../contexts/UserContext";
-import useBridgeConnector from "../../bridge/useBridgeConnector";
-import {
-  PROJECT_AND_WORK_TASKS_QUERY,
-  ProjectAndWorkTasksResponse,
-  SET_CURRENT_WORK_TASK_MUTATION,
-  WorkTask,
-  ProjectAndWorkTasks,
-} from "./WorkTasksGql";
-import SelectMenu, { SelectOption } from "../../components/SelectMenu";
-import SelectListView, { BodyItem } from "../../components/SelectListView";
+import LabelContainer from "../../components/LabelContainer";
+import SelectMenu from "../../components/SelectMenu";
 import DefaultButton from "../../components/DefaultButton";
-import Loading from "../../components/Loading";
-import { toast } from "react-toastify";
-
-interface WorkTaskBodyItem extends BodyItem {
-  collectionId: string;
-  workTask: WorkTask;
-}
-
-function createProjectSelectOptions(
-  projects: ProjectAndWorkTasks[]
-): SelectOption[] {
-  return projects.map<SelectOption>((x) => {
-    return { text: x.name ?? "", value: x.mRID ?? "" };
-  });
-}
-
-function createWorkTaskBodyItems(
-  projects: ProjectAndWorkTasks[]
-): WorkTaskBodyItem[] {
-  return projects.flatMap((p) => {
-    return p.workTasks.map<WorkTaskBodyItem>((w) => {
-      return {
-        collectionId: p.mRID ?? "",
-        id: w.mRID ?? "",
-        rows: [
-          {
-            id: 0,
-            value: w.name ? w.name : "",
-          },
-          {
-            id: 1,
-            value: w.centralOfficeArea ? w.centralOfficeArea : "",
-          },
-          {
-            id: 2,
-            value: w.flexPointArea ? w.flexPointArea : "",
-          },
-          {
-            id: 3,
-            value: w.splicePointArea ? w.splicePointArea : "",
-          },
-          {
-            id: 4,
-            value: w.technology ? w.technology : "",
-          },
-          {
-            id: 5,
-            value: w.workTaskType ? w.workTaskType : "",
-          },
-          {
-            id: 6,
-            value: w.addressString ? w.addressString : "",
-          },
-          {
-            id: 7,
-            value: w.status ? w.status : "",
-          },
-        ],
-        workTask: w,
-      };
-    });
-  });
-}
+import SelectListView from "../../components/SelectListView";
 
 function WorkTasks() {
   const { t } = useTranslation();
-  const { keycloak } = useKeycloak();
-  const { reloadUserWorkTask } = useContext(UserContext);
-  const { panToCoordinate } = useBridgeConnector();
-  const [selectedProject, setSelectedProject] = useState<string>();
-  const [selectedWorkTask, setSelectedWorkTask] = useState<string>();
-  const [projects, setProjects] = useState<ProjectAndWorkTasks[]>([]);
-  const [projectsResponse] = useQuery<ProjectAndWorkTasksResponse>({
-    query: PROJECT_AND_WORK_TASKS_QUERY,
-  });
-  const [setCurrentWorkTaskResult, setCurrentWorkTask] = useMutation(
-    SET_CURRENT_WORK_TASK_MUTATION
-  );
-
-  useEffect(() => {
-    if (!projectsResponse?.data) return;
-    const projectsAndWorksTasks =
-      projectsResponse.data.workService.projectsAndWorkTasks;
-    setProjects(projectsAndWorksTasks);
-    if (!selectedProject) {
-      setSelectedProject(projectsAndWorksTasks[0].mRID ?? "");
-    }
-  }, [projectsResponse, selectedProject, setSelectedProject, setProjects]);
-
-  useEffect(() => {
-    setSelectedWorkTask("");
-  }, [selectedProject, setSelectedProject]);
-
-  useEffect(() => {
-    if (!setCurrentWorkTaskResult?.data) return;
-    if (!setCurrentWorkTaskResult.error) {
-      toast.success(t("Work task is now added to user"));
-      reloadUserWorkTask();
-    } else {
-      toast.error(setCurrentWorkTaskResult.error.message);
-    }
-  }, [setCurrentWorkTaskResult, t, reloadUserWorkTask]);
-
-  const workTaskBodyItems = useMemo(
-    () =>
-      createWorkTaskBodyItems(projects).filter(
-        (x) => x.collectionId === selectedProject
-      ),
-    [projects, selectedProject]
-  );
-
-  const projectSelectOptions = useMemo(
-    () => createProjectSelectOptions(projects),
-    [projects]
-  );
-
-  const selectItem = useCallback(
-    (x: BodyItem) => {
-      setSelectedWorkTask(x.id as string);
-    },
-    [setSelectedWorkTask]
-  );
-
-  const pickWorkTask = useCallback(() => {
-    if (!selectedWorkTask || !keycloak.profile?.username) return;
-    setCurrentWorkTask({
-      userName: keycloak.profile?.username,
-      workTaskId: selectedWorkTask,
-    });
-  }, [setCurrentWorkTask, selectedWorkTask, keycloak.profile?.username]);
-
-  const panToAddress = () => {
-    const workTask = workTaskBodyItems.find(
-      (x) => x.workTask.mRID === selectedWorkTask
-    )?.workTask;
-
-    if (!workTask) {
-      toast.warning(t("Please select a work task"));
-    }
-    if (!workTask?.geometry?.coordinates) {
-      toast.warning(t("The work task has no coordinates"));
-      return;
-    }
-
-    panToCoordinate(workTask.geometry.coordinates);
-    toast.success(t("Pan/Zoom to address"));
-  };
-
-  if (projectsResponse.fetching) return <Loading />;
 
   return (
-    <div>
-      <div className="full-row">
-        <SelectMenu
-          onSelected={(x) => setSelectedProject(x as string)}
-          selected={selectedProject}
-          options={projectSelectOptions}
-        />
+    <div className="work-tasks">
+      <div className="full-row full-row gap-default">
+        <LabelContainer text={t("PROJECT")}>
+          <SelectMenu
+            options={[]}
+            removePlaceHolderOnSelect
+            onSelected={(x) => {}}
+            selected={""}
+            enableSearch={true}
+          />
+        </LabelContainer>
+        <LabelContainer text={t("WORK_TASK_TYPE")}>
+          <SelectMenu
+            options={[]}
+            removePlaceHolderOnSelect
+            onSelected={(x) => {}}
+            selected={""}
+            enableSearch={true}
+          />
+        </LabelContainer>
+        <LabelContainer text={t("WORK_TASK_STATUS")}>
+          <SelectMenu
+            options={[]}
+            removePlaceHolderOnSelect
+            onSelected={(x) => {}}
+            selected={""}
+            enableSearch={true}
+          />
+        </LabelContainer>
       </div>
       <div className="full-row">
         <SelectListView
           headerItems={[
-            t("Name"),
-            t("Central office area"),
-            t("Flex point area"),
-            t("Splice point area"),
-            t("Technology"),
-            t("Work task type"),
-            t("Address"),
-            t("Status"),
+            t("PROJECT_NUMBER"),
+            t("PROJECT_NAME"),
+            t("WORK_TASK_NUMBER"),
+            t("WORK_TASK_TYPE"),
+            t("WORK_TASK_STATUS"),
+            t("WORK_TASK_NAME"),
+            t("SUBTASK"),
+            t("OWNER"),
+            t("CREATED_DATE"),
+            t("INSTALLATION_NUMBER"),
+            t("MODIFIED_BY"),
           ]}
-          bodyItems={workTaskBodyItems}
-          selectItem={selectItem}
-          selected={selectedWorkTask}
+          bodyItems={[]}
+          selectItem={() => {}}
+          selected={""}
         />
       </div>
-      <div className="full-row">
-        <DefaultButton
-          innerText={t("Pick work task")}
-          onClick={pickWorkTask}
-          disabled={!selectedWorkTask}
-        />
+      <div className="full-row gap-default">
+        <DefaultButton innerText={t("Pick work task")} onClick={() => {}} />
         <DefaultButton
           innerText={t("Pan/Zoom to address")}
-          onClick={panToAddress}
-          disabled={!selectedWorkTask}
+          onClick={() => {}}
         />
       </div>
     </div>
