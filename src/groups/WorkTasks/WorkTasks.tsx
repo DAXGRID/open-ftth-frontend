dcffabfb49d13c3d568e5b0bf682700748939094
Rune Nielsen <runenielsen@runbox.com>
2022-06-14 13:22:24 +0200

    can now pick select filters

--- src/groups/WorkTasks/WorkTasks.tsx
+++ src/groups/WorkTasks/WorkTasks.tsx
@@ -1,39 +1,190 @@
+import { useEffect, useMemo, useReducer } from "react";
+import { useClient } from "urql";
 import { useTranslation } from "react-i18next";
 import LabelContainer from "../../components/LabelContainer";
-import SelectMenu from "../../components/SelectMenu";
+import SelectMenu, { SelectOption } from "../../components/SelectMenu";
 import DefaultButton from "../../components/DefaultButton";
-import SelectListView from "../../components/SelectListView";
+import SelectListView, { BodyItem } from "../../components/SelectListView";
+import { getWorksTasks, WorkTask } from "./WorkTasksGql";
+
+function mapProjectSelectOptions(workTasks: WorkTask[]): SelectOption[] {
+  return workTasks.reduce<SelectOption[]>((acc, x) => {
+    const notInCollection = !!!acc.find((y) => y.value === x.projectNumber);
+    return notInCollection && x.projectName && x.projectNumber
+      ? [
+          ...acc,
+          {
+            text: `${x.projectNumber} ${x.projectName}`,
+            value: x.projectNumber,
+            key: x.projectNumber,
+          },
+        ]
+      : acc;
+  }, []);
+}
+
+function mapWorkTaskType(workTasks: WorkTask[]): SelectOption[] {
+  return workTasks.reduce<SelectOption[]>((acc, x) => {
+    const notInCollection = !!!acc.find((y) => y.value === x.type);
+    return notInCollection && x.type
+      ? [
+          ...acc,
+          {
+            text: x.type,
+            value: x.type,
+            key: x.type,
+          },
+        ]
+      : acc;
+  }, []);
+}
+
+function mapWorkTaskStatus(workTasks: WorkTask[]): SelectOption[] {
+  return workTasks.reduce<SelectOption[]>((acc, x) => {
+    const notInCollection = !!!acc.find((y) => y.value === x.status);
+    return notInCollection && x.status
+      ? [
+          ...acc,
+          {
+            text: x.status,
+            value: x.status,
+            key: x.status,
+          },
+        ]
+      : acc;
+  }, []);
+}
+
+interface State {
+  workTasks: WorkTask[];
+  projectNumberFilter: string;
+  workTaskTypeFilter: string;
+  workTaskStatusFilter: string;
+}
+
+type Action =
+  | { type: "setWorkTasks"; workTasks: WorkTask[] }
+  | { type: "setProjectNumberFilter"; projectNumber: string }
+  | { type: "setWorkTaskTypeFilter"; workTaskType: string }
+  | { type: "setWorkTaskStatusFilter"; workTaskStatus: string };
+
+const initialState: State = {
+  workTasks: [],
+  projectNumberFilter: "ALL",
+  workTaskTypeFilter: "ALL",
+  workTaskStatusFilter: "ALL",
+};
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "setWorkTasks":
+      return { ...initialState, workTasks: action.workTasks };
+    case "setProjectNumberFilter":
+      return { ...state, projectNumberFilter: action.projectNumber };
+    case "setWorkTaskTypeFilter":
+      return { ...state, workTaskTypeFilter: action.workTaskType };
+    case "setWorkTaskStatusFilter":
+      return { ...state, workTaskStatusFilter: action.workTaskStatus };
+    default:
+      throw new Error(`No action found.`);
+  }
+}
 
 function WorkTasks() {
   const { t } = useTranslation();
+  const client = useClient();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: "setWorkTasks", workTasks: getWorksTasks(client) });
+  }, [client, dispatch]);
+
+  const projectSelectOptions = useMemo(() => {
+    const defaultOptions: SelectOption[] = [
+      { text: t("ALL"), value: "ALL", key: "ALL" },
+      {
+        text: t("NO_ASSOCIATED_PROJECT"),
+        value: "NO_ASSOCIATED_PROJECT",
+        key: "NO_ASSOCIATED_PROJECT",
+      },
+    ];
+    return state.workTasks
+      ? [
+          ...defaultOptions,
+          ...mapProjectSelectOptions(state.workTasks).sort((x, y) =>
+            x.text > y.text ? 1 : -1
+          ),
+        ]
+      : [];
+  }, [state.workTasks, t]);
+
+  const typeSelectOptions = useMemo(() => {
+    const defaultOptions: SelectOption[] = [
+      { text: t("ALL"), value: "ALL", key: "ALL" },
+    ];
+    return state.workTasks
+      ? [
+          ...defaultOptions,
+          ...mapWorkTaskType(state.workTasks).sort((x, y) =>
+            x.text > y.text ? 1 : -1
+          ),
+        ]
+      : [];
+  }, [state.workTasks, t]);
+
+  const statusSelectOptions = useMemo(() => {
+    const defaultOptions: SelectOption[] = [
+      { text: t("ALL"), value: "ALL", key: "ALL" },
+    ];
+    return state.workTasks
+      ? [
+          ...defaultOptions,
+          ...mapWorkTaskStatus(state.workTasks).sort((x, y) =>
+            x.text > y.text ? 1 : -1
+          ),
+        ]
+      : [];
+  }, [state.workTasks, t]);
 
   return (
     <div className="work-tasks">
       <div className="full-row full-row gap-default">
         <LabelContainer text={t("PROJECT")}>
           <SelectMenu
-            options={[]}
-            removePlaceHolderOnSelect
-            onSelected={(x) => {}}
-            selected={""}
+            options={projectSelectOptions}
+            onSelected={(x) =>
+              dispatch({
+                type: "setProjectNumberFilter",
+                projectNumber: x as string,
+              })
+            }
+            selected={state.projectNumberFilter ?? ""}
             enableSearch={true}
           />
         </LabelContainer>
         <LabelContainer text={t("WORK_TASK_TYPE")}>
           <SelectMenu
-            options={[]}
-            removePlaceHolderOnSelect
-            onSelected={(x) => {}}
-            selected={""}
+            options={typeSelectOptions}
+            onSelected={(x) =>
+              dispatch({
+                type: "setWorkTaskTypeFilter",
+                workTaskType: x as string,
+              })
+            }
+            selected={state.workTaskTypeFilter}
             enableSearch={true}
           />
         </LabelContainer>
         <LabelContainer text={t("WORK_TASK_STATUS")}>
           <SelectMenu
-            options={[]}
-            removePlaceHolderOnSelect
-            onSelected={(x) => {}}
-            selected={""}
+            options={statusSelectOptions}
+            onSelected={(x) =>
+              dispatch({
+                type: "setWorkTaskStatusFilter",
+                workTaskStatus: x as string,
+              })
+            }
+            selected={state.workTaskStatusFilter}
             enableSearch={true}
           />
         </LabelContainer>
--- src/groups/WorkTasks/WorkTasksGql.ts
+++ src/groups/WorkTasks/WorkTasksGql.ts
@@ -1,69 +1,137 @@
-interface Geometry {
-  coordinates: string;
-  type: string;
+import { Client } from "urql";
+
+const exampleWorkTasks: WorkTask[] = [
+  {
+    projectNumber: "P12345",
+    projectName: "Andeby",
+    number: "W000001",
+    type: "Gadeskab",
+    status: "Oprettet",
+    name: "Nyt skabsomraade i odder nybolig vej",
+    subtask: null,
+    owner: "Hans",
+    createdDate: "01-06-2021",
+    installationNumber: null,
+    modifiedBy: null,
+    geometry: null,
+  },
+  {
+    projectNumber: "P12345",
+    projectName: "Andeby",
+    number: "W000002",
+    type: "Gadeskab",
+    status: "Fuldfoert",
+    name: "Nyt skabsomraade syd",
+    subtask: "Etape 1",
+    owner: "Hans",
+    createdDate: "01-06-2021",
+    installationNumber: null,
+    modifiedBy: null,
+    geometry: null,
+  },
+  {
+    projectNumber: "P12345",
+    projectName: "Andeby",
+    number: "W000003",
+    type: "Gadeskab",
+    status: "Oprettet",
+    name: "Nyt skabsomraade syd",
+    subtask: "Etape 2",
+    owner: "Hans",
+    createdDate: "04-06-2021",
+    installationNumber: null,
+    modifiedBy: "Susie",
+    geometry: null,
+  },
+  {
+    projectNumber: "P22222234",
+    projectName: "Boligby",
+    number: "W000004",
+    type: "Installation i foreninger",
+    status: "Igangvaerende",
+    name: "Bygning 12A",
+    subtask: null,
+    owner: "Bent",
+    createdDate: "23-01-2022",
+    installationNumber: null,
+    modifiedBy: null,
+    geometry: null,
+  },
+  {
+    projectNumber: "P22222234",
+    projectName: "Boligby",
+    number: "W000004",
+    type: "Installation i foreninger",
+    status: "Igangvaerende",
+    name: "Bygning 12A",
+    subtask: null,
+    owner: "Bent",
+    createdDate: "23-01-2022",
+    installationNumber: null,
+    modifiedBy: null,
+    geometry: null,
+  },
+  {
+    projectNumber: null,
+    projectName: null,
+    number: "W000010",
+    type: "Eftertilmelding Auto",
+    status: "Oprettet",
+    name: null,
+    subtask: null,
+    owner: null,
+    createdDate: "10-02-2022",
+    installationNumber: "12345",
+    modifiedBy: null,
+    geometry: null,
+  },
+  {
+    projectNumber: null,
+    projectName: null,
+    number: "W000011",
+    type: "Eftertilmelding Manuel",
+    status: "Oprettet",
+    name: null,
+    subtask: null,
+    owner: null,
+    createdDate: "10-02-2022",
+    installationNumber: "23456",
+    modifiedBy: "Jane",
+    geometry: null,
+  },
+];
+
+// TODO use client.
+export function getWorksTasks(_: Client): WorkTask[] {
+  return exampleWorkTasks;
 }
 
 export interface WorkTask {
-  mRID: string;
-  workTaskType: string;
-  name?: string;
-  status?: string;
-  addressString?: string;
-  centralOfficeArea?: string;
-  flexPointArea?: string;
-  splicePointArea?: string;
-  installationId?: string;
-  technology?: string;
-  geometry?: Geometry;
+  projectNumber: string | null;
+  projectName: string | null;
+  number: string;
+  type: string;
+  status: string;
+  name: string | null;
+  subtask: string | null;
+  owner: string | null;
+  createdDate: string;
+  installationNumber: string | null;
+  modifiedBy: string | null;
+  geometry: {
+    coordinates: string;
+    type: string;
+  } | null;
 }
 
-export interface ProjectAndWorkTasks {
-  mRID?: string;
-  name?: string;
-  workTasks: WorkTask[];
-}
-
-interface WorkService {
-  projectsAndWorkTasks: ProjectAndWorkTasks[];
-}
-
-export interface ProjectAndWorkTasksResponse {
-  workService: WorkService;
-}
-
-export const PROJECT_AND_WORK_TASKS_QUERY = `query {
-  workService {
-    projectsAndWorkTasks {
-      mRID
-      name
-      workTasks {
-        mRID
-        workTaskType
-        name
-        status
-        addressString
-        centralOfficeArea
-        flexPointArea
-        splicePointArea
-        installationId
-        technology
-        geometry {
-          coordinates
-          type
-        }
-      }
-    }
-  }
-}`;
-
-export const SET_CURRENT_WORK_TASK_MUTATION = `mutation ($userName: String!, $workTaskId: ID!) {
-  userContext {
-    setCurrentWorkTask(userName: $userName, workTaskId: $workTaskId)
-    {
-      currentWorkTask {
-        __typename
-      }
-      userName
-    }
-  }
-}`;
+// export const SET_CURRENT_WORK_TASK_MUTATION = `mutation ($userName: String!, $workTaskId: ID!) {
+//   userContext {
+//     setCurrentWorkTask(userName: $userName, workTaskId: $workTaskId)
+//     {
+//       currentWorkTask {
+//         __typename
+//       }
+//       userName
+//     }
+//   }
+// }`;
