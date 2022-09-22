07c6bda873bded49ac16af5fa911fd23565c6410
Rune Nielsen <runenielsen@runbox.com>
2022-09-22 16:43:33 +0200

    add copy node values and description to clipboard

--- src/groups/RouteNetworkDiagram/OutageView/OutageView.tsx
+++ src/groups/RouteNetworkDiagram/OutageView/OutageView.tsx
@@ -7,6 +7,7 @@ import TreeViewCheckbox, {
 } from "../../../components/TreeViewCheckbox";
 import DefaultButton from "../../../components/DefaultButton";
 import SelectMenu, { SelectOption } from "../../../components/SelectMenu";
+import { toast } from "react-toastify";
 
 function convertToTreeNodes(node: Node): TreeNode {
   const children: TreeNode[] = [];
@@ -49,27 +50,31 @@ function mapWorkTasksToOptions(workTasks: WorkTask[]): SelectOption[] {
   }));
 }
 
-function selectedNodeValues(node: TreeNode): string[] {
-  let result: string[] = [];
+function selectedNodes(node: TreeNode): TreeNode[] {
+  let result: TreeNode[] = [];
 
-  if (node.selected && node.value) {
-    result.push(node.value);
+  if (node.selected) {
+    result.push(node);
   }
 
   if (node.nodes) {
     node.nodes.forEach((x) => {
-      result = [...result, ...selectedNodeValues(x)];
+      result = [...result, ...selectedNodes(x)];
     });
   }
 
   return result;
 }
 
+function formatNodesClipboard(nodes: TreeNode[]): string {
+  return nodes.map((x) => `${x.value}\t${x.description}`).join("\n");
+}
+
 function OutageView() {
   const client = useClient();
   const { t } = useTranslation();
   const [node, setNode] = useState<TreeNode | null>(null);
-  const [selectedWorkTask, setSelctedWorkTask] = useState<string>("");
+  const [selectedWorkTask, setSelectedWorkTask] = useState<string>("");
   const [workTasks, setWorkTasks] = useState<WorkTask[]>([]);
 
   useEffect(() => {
@@ -100,9 +105,32 @@ function OutageView() {
   };
 
   const send = () => {
-    console.log("The button is clicked.");
     if (node) {
-      console.log(selectedNodeValues(node));
+      console.log(
+        "Selected nodes with values.",
+        selectedNodes(node)
+          .filter((x) => x.value)
+          .map((x) => x.value)
+      );
+    }
+  };
+
+  const copyToClipboard = () => {
+    if (node) {
+      var clipboardText = formatNodesClipboard(
+        selectedNodes(node).filter((x) => x.value)
+      );
+      navigator.clipboard.writeText(clipboardText).then(
+        () => {
+          toast.success(t("COPIED_TO_CLIPBOARD"));
+        },
+        (err) => {
+          toast.error(t("COULD_NOT_COPY_TO_CLIPBOARD"));
+          console.error(err);
+        }
+      );
+    } else {
+      throw Error("Something is wrong, no node is found.");
     }
   };
 
@@ -121,14 +149,22 @@ function OutageView() {
       </div>
       <div className="full-row">
         <SelectMenu
-          onSelected={(x) => setSelctedWorkTask(x as string)}
+          onSelected={(x) => setSelectedWorkTask(x as string)}
           removePlaceHolderOnSelect
           selected={selectedWorkTask}
           options={workTaskOptions}
         />
       </div>
-      <div className="full-row">
-        <DefaultButton onClick={() => send()} innerText={t("SEND")} />
+      <div className="full-row gap-default">
+        <DefaultButton
+          disabled={selectedWorkTask === ""}
+          onClick={() => send()}
+          innerText={t("SEND")}
+        />
+        <DefaultButton
+          onClick={() => copyToClipboard()}
+          innerText={t("COPY_TO_CLIPBOARD")}
+        />
       </div>
     </div>
   );
