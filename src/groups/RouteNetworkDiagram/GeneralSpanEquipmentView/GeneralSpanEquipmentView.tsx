c731d6542def9e1eb505301a99ad46a186b7b072
Rune Nielsen <runenielsen@runbox.com>
2024-03-11 16:31:28 +0100

    add general span equipment view (#516)

---
+++ src/groups/RouteNetworkDiagram/GeneralSpanEquipmentView/GeneralSpanEquipmentView.tsx
@@ -0,0 +1,165 @@
+import { useEffect, useState } from "react";
+import { useClient } from "urql";
+import { useTranslation } from "react-i18next";
+import LabelContainer from "../../../components/LabelContainer";
+import TextBox from "../../../components/TextBox";
+import {
+  getSpanEquipmentDetails,
+  SpanEquipment,
+} from "./GeneralSpanEquipmentViewGql";
+
+interface GeneralSpanEquipmentViewProps {
+  spanEquipmentId: string;
+}
+
+interface AccessAddress {
+  postDistrictCode: string;
+  postDistrict: string;
+  roadName: string;
+  houseNumber: string;
+}
+
+interface UnitAddress {
+  externalId: string;
+  floorName: string;
+  suitName: string;
+}
+
+function addressDisplayText(
+  accessAddress: AccessAddress | null,
+  unitAddress: UnitAddress | null,
+): string {
+  if (!accessAddress) {
+    return "";
+  }
+
+  let unitAddressText = `${unitAddress?.floorName ?? ""} ${
+    unitAddress?.suitName ?? ""
+  }`.trim();
+
+  if (unitAddressText.length > 0) {
+    return `${accessAddress.roadName} ${accessAddress.houseNumber}, ${unitAddressText}, ${accessAddress.postDistrict} ${accessAddress.postDistrictCode}`;
+  } else {
+    return `${accessAddress.roadName} ${accessAddress.houseNumber}, ${accessAddress.postDistrict} ${accessAddress.postDistrictCode}`;
+  }
+}
+
+function GeneralSpanEquipmentView({
+  spanEquipmentId,
+}: GeneralSpanEquipmentViewProps) {
+  const client = useClient();
+  const { t } = useTranslation();
+  const [spanEquipment, setSpanEquipment] = useState<SpanEquipment | null>(
+    null,
+  );
+
+  useEffect(() => {
+    getSpanEquipmentDetails(client, spanEquipmentId)
+      .then((x) => {
+        let spanEquipment = x.data?.utilityNetwork.spanEquipment;
+        if (spanEquipment) {
+          setSpanEquipment(spanEquipment);
+        } else {
+          console.error("Could not receive span equipment details.");
+        }
+      })
+      .catch((x) => {
+        console.error(x);
+      });
+  }, [client, spanEquipmentId]);
+
+  if (!spanEquipment) {
+    return <></>;
+  }
+
+  return (
+    <div className="general-span-equipment-view">
+      <div className="general-view-container">
+        <div className="general-view-header">
+          <p>{spanEquipment.name}</p>
+        </div>
+        <div className="general-span-equipment-view-body">
+          <div className="block">
+            <div className="full-row">
+              <LabelContainer text={t("NAME")}>
+                <TextBox
+                  value={spanEquipment.name ?? ""}
+                  setValue={() => {}}
+                  disabled={true}
+                />
+              </LabelContainer>
+            </div>
+            <div className="full-row">
+              <LabelContainer text={t("SPECIFCATION_DESCRIPTION")}>
+                <TextBox
+                  value={spanEquipment?.specification?.description ?? ""}
+                  setValue={() => {}}
+                  disabled={true}
+                />
+              </LabelContainer>
+            </div>
+            <div className="full-row">
+              <LabelContainer text={t("MARKING_COLOR")}>
+                <TextBox
+                  value={spanEquipment.markingInfo?.markingColor ?? ""}
+                  setValue={() => {}}
+                  disabled={true}
+                />
+              </LabelContainer>
+            </div>
+            <div className="full-row">
+              <LabelContainer text={t("MARKING_TEXT")}>
+                <TextBox
+                  value={spanEquipment.markingInfo?.markingText ?? ""}
+                  setValue={() => {}}
+                  disabled={true}
+                />
+              </LabelContainer>
+            </div>
+          </div>
+          {spanEquipment?.addressInfo && (
+            <div className="block">
+              <p className="block-title block-title__underline block-title__big">
+                {t("ADDRESS_INFORMATION")}
+              </p>
+              <div className="full-row">
+                <LabelContainer text={t("ADDRESS")}>
+                  <TextBox
+                    value={addressDisplayText(
+                      spanEquipment.addressInfo.accessAddress ?? null,
+                      spanEquipment.addressInfo?.unitAddress ?? null,
+                    )}
+                    setValue={() => {}}
+                    disabled={true}
+                  />
+                </LabelContainer>
+              </div>
+              <div className="full-row">
+                <LabelContainer text={t("ADDITIONAL_ADDRESS_INFORMATION")}>
+                  <TextBox
+                    value={spanEquipment.addressInfo?.remark ?? ""}
+                    setValue={() => {}}
+                    disabled={true}
+                  />
+                </LabelContainer>
+              </div>
+              <div className="full-row">
+                <LabelContainer text={t("ADDRESS_ID")}>
+                  <TextBox
+                    value={
+                      spanEquipment.addressInfo.unitAddress?.externalId ?? ""
+                    }
+                    setValue={() => {}}
+                    disabled={true}
+                  />
+                </LabelContainer>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GeneralSpanEquipmentView;
---
+++ src/groups/RouteNetworkDiagram/GeneralSpanEquipmentView/GeneralSpanEquipmentViewGql.ts
@@ -0,0 +1,81 @@
+import { Client } from "urql";
+
+export function getSpanEquipmentDetails(
+  client: Client,
+  spanEquipmentOrSegmentId: string
+) {
+  return client
+    .query<QuerySpanEquipmentDetailsResponse>(
+      QUERY_SPAN_EQUIPMENT_DETAILS,
+      {
+        spanEquipmentOrSegmentId: spanEquipmentOrSegmentId,
+      }
+    )
+    .toPromise();
+}
+
+
+interface QuerySpanEquipmentDetailsResponse {
+  utilityNetwork: {
+    spanEquipment: SpanEquipment;
+  };
+}
+
+export interface SpanEquipment {
+  id: string;
+  name: string;
+  specification: {
+    description: string;
+  };
+  markingInfo: {
+    markingColor: string;
+    markingText: string;
+  };
+  addressInfo?: {
+    remark?: string;
+    accessAddress?: {
+      postDistrictCode: string;
+      postDistrict: string;
+      houseNumber: string;
+      roadName: string;
+    };
+    unitAddress?: {
+      externalId: string;
+      floorName: string;
+      suitName: string;
+    };
+  };
+}
+
+const QUERY_SPAN_EQUIPMENT_DETAILS = `
+query ($spanEquipmentOrSegmentId: ID!) {
+  utilityNetwork {
+    spanEquipment(spanEquipmentOrSegmentId: $spanEquipmentOrSegmentId) {
+      id
+      name
+      description
+      specification {
+        description
+      }
+      markingInfo {
+        markingColor
+        markingText
+      }
+      addressInfo {
+        remark
+        accessAddress {
+          postDistrictCode
+          postDistrict
+          houseNumber
+          roadName
+        }
+        unitAddress {
+          externalId
+          floorName
+          suitName
+        }
+      }
+    }
+  }
+}
+`
