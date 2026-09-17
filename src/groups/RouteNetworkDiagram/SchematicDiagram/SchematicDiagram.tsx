e8114c9a36972662463b7c9ce3a44bd975de2f0a
Rune Nielsen <runenielsen@runbox.com>
2026-09-17 08:34:10 +0200

    downgrade maplibre issues with freezing browser (#787)

--- src/groups/RouteNetworkDiagram/SchematicDiagram/SchematicDiagram.tsx
+++ src/groups/RouteNetworkDiagram/SchematicDiagram/SchematicDiagram.tsx
@@ -7,7 +7,6 @@ import {
   NavigationControl,
   MapDataEvent,
   MapMouseEvent,
-  setWorkerUrl,
 } from "maplibre-gl";
 import { DiagramContext } from "../DiagramContext";
 import { useContext, useEffect, useLayoutEffect, useRef } from "react";
@@ -25,7 +24,6 @@ import {
   fiberCableUnderLayer,
   freeRackSpaceSelect,
 } from "./diagramLayer";
-import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url";
 
 interface Envelope {
   minX: number;
@@ -62,8 +60,6 @@ interface SchematicPosition {
   envelope: Envelope;
   zoom: number;
 }
-
-setWorkerUrl(workerUrl);
 
 const loadDiagram = (map: Map, diagramObjects: Diagram[]) => {
   const t: { [id: string]: Feature[] } = {};
